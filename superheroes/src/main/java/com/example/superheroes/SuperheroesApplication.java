d9e1bce83d9faab06087896d093eed4a41a1455c
Taimoor <taimoor1616@gmail.com>
2023-07-28 13:43:28 +0100

    QL-5

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -3,6 +3,7 @@ package com.example.superheroes;
 import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
 import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,7 @@ import java.util.List;
 public class SuperheroesApplication implements CommandLineRunner {
 
 	@Autowired
-	HeroRepository heroRepository;
+	HeroService heroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -22,10 +23,12 @@ public class SuperheroesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("C%");
+		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
 		for (Hero hero : heroes) {
 			System.out.println(hero);
 		}
 	}
+
+
 
 }
