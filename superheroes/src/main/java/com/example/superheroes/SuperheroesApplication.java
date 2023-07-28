2ce169cb90feecca975e3f547f57dc61f0892a36
Taimoor <taimoor1616@gmail.com>
2023-07-28 14:18:12 +0100

    QL-6

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -12,23 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
-
-	@Autowired
-	HeroService heroService;
+public class SuperheroesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
-		for (Hero hero : heroes) {
-			System.out.println(hero);
-		}
-	}
-
-
 
 }
